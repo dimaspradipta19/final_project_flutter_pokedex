f5ba57cef4ae8b12d12c33d612535cb951c78d65
Dimas Pradipta Putra <dimaspradiptaputra@Dimass-MacBook-Air.local>
2024-01-16 22:59:40 +0700

    [fix] handling error when API cant find the specific pokemon in searchscreen

--- lib/data/services/get_all_pokemon_service.dart
+++ lib/data/services/get_all_pokemon_service.dart
@@ -9,7 +9,7 @@ class GetAllPokemonService {
 
   final Dio _dio = Dio();
 
-  Future<PokemonAllModel?> getAllPokemon() async {
+  Future<PokemonAllModel> getAllPokemon() async {
     try {
       var response = await _dio.get(baseUrl);
 
@@ -17,11 +17,46 @@ class GetAllPokemonService {
         var resultPokemon = PokemonAllModel.fromJson(response.data);
         return resultPokemon;
       } else {
-        log("Error ${response.statusCode}");
+        return _handleError(response);
       }
     } catch (e) {
-      log(e.toString());
+      return _handleError(e);
     }
-    return null;
+  }
+
+  _handleError(error) {
+    String errorDesc = "";
+    DioException dioError = error as DioException;
+    switch (dioError.type) {
+      // ignore: deprecated_member_use
+      case DioExceptionType.connectionTimeout:
+        errorDesc = "Connection to server is timeout";
+        break;
+      case DioExceptionType.sendTimeout:
+        errorDesc = "Send timeout in connection to server";
+        break;
+      case DioExceptionType.receiveTimeout:
+        errorDesc = "Received timeout in connection to server";
+        break;
+      case DioExceptionType.badResponse:
+        errorDesc =
+            "Received server error status code ${dioError.response!.statusCode}";
+        break;
+      case DioExceptionType.cancel:
+        errorDesc = "Connection to server was cancelled";
+        break;
+      case DioExceptionType.unknown:
+        errorDesc = "Unknown error connection to server ";
+        break;
+      default:
+    }
+    final Response errorResponse = Response(
+      requestOptions: RequestOptions(path: ""),
+      data: errorDesc,
+      statusMessage: (dioError.response!).toString(),
+    );
+    // You might want to log the error message here
+    log("Error: $errorDesc");
+    return errorResponse;
   }
 }
--- lib/data/services/get_name_pokemon_service.dart
+++ lib/data/services/get_name_pokemon_service.dart
@@ -9,7 +9,7 @@ class GetNamePokemonService {
 
   final Dio _dio = Dio();
 
-  Future<NamePokemonModel?> getNamePokemon(String namePokemon) async {
+  Future<NamePokemonModel> getNamePokemon(String namePokemon) async {
     try {
       Response response = await _dio.get("$baseUrl$namePokemon");
 
@@ -17,11 +17,46 @@ class GetNamePokemonService {
         var resultNamePokemon = NamePokemonModel.fromJson(response.data);
         return resultNamePokemon;
       } else {
-        log("Error ${response.statusCode}");
+        return _handleError(response);
       }
-    } catch (e) {
-      log("Error ni bro di service name pokemon ${e.toString()}");
+    } on DioException catch (e) {
+      return _handleError(e);
     }
-    return null;
+  }
+
+  _handleError(error) {
+    String errorDesc = "";
+    DioException dioError = error as DioException;
+    switch (dioError.type) {
+      // ignore: deprecated_member_use
+      case DioExceptionType.connectionTimeout:
+        errorDesc = "Connection to server is timeout";
+        break;
+      case DioExceptionType.sendTimeout:
+        errorDesc = "Send timeout in connection to server";
+        break;
+      case DioExceptionType.receiveTimeout:
+        errorDesc = "Received timeout in connection to server";
+        break;
+      case DioExceptionType.badResponse:
+        errorDesc =
+            "Received server error status code ${dioError.response!.statusCode}";
+        break;
+      case DioExceptionType.cancel:
+        errorDesc = "Connection to server was cancelled";
+        break;
+      case DioExceptionType.unknown:
+        errorDesc = "Unknown error connection to server ";
+        break;
+      default:
+    }
+    final Response errorResponse = Response(
+      requestOptions: RequestOptions(path: ""),
+      data: errorDesc,
+      statusMessage: (dioError.response!).toString(),
+    );
+    // You might want to log the error message here
+    log("Error: $errorDesc");
+    return errorResponse;
   }
 }
